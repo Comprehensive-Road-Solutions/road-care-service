7d4d5151a60fe1721423b417e8b85e2e864e8664
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 18:20:56 -0500

    feat(publishing): added services for district and department.

---
+++ RoadCareService/Publishing/Domain/Services/Department/IDepartmentQueryService.cs
@@ -0,0 +1,12 @@
+﻿using RoadCareService.Publishing.Domain.Model.Queries.Department;
+
+namespace RoadCareService.Publishing.Domain.Services.Department
+{
+    public interface IDepartmentQueryService
+    {
+        Task<IEnumerable<Model.Entities.Department>> Handle
+            (GetAllDepartmentsQuery query);
+        Task<IEnumerable<Model.Entities.Department>> Handle
+            (GetDepartmentsByIdQuery query);
+    }
+}
---
+++ RoadCareService/Publishing/Domain/Services/District/IDistrictQueryService.cs
@@ -0,0 +1,10 @@
+﻿using RoadCareService.Publishing.Domain.Model.Queries.District;
+
+namespace RoadCareService.Publishing.Domain.Services.District
+{
+    public interface IDistrictQueryService
+    {
+        Task<IEnumerable<Model.Entities.District>> Handle
+            (GetDistrictsByDepartmentsIdQuery query);
+    }
+}
