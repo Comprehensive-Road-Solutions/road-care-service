ba8b3044306e8302243f6a0c42620f51e0aaf048
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 18:09:09 -0500

    feat(publishing): added repositories for department and district.

---
+++ RoadCareService/Publishing/Domain/Repositories/IDistrictRepository.cs
@@ -0,0 +1,7 @@
+﻿using RoadCareService.Publishing.Domain.Model.Entities;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Publishing.Domain.Repositories
+{
+    public interface IDistrictRepository : IBaseRepository<District> { }
+}
