05a9c0f63d6c27a1b5300676de64c2e8169fc89b
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 17:30:49 -0500

    feat(publishing): added queries for publication.

---
+++ RoadCareService/Publishing/Domain/Model/Queries/GetPublicationsByDepartmentsIdAndDistrictsIdQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace RoadCareService.Publishing.Domain.Model.Queries
+{
+    public record GetPublicationsByDepartmentsIdAndDistrictsIdQuery(int DepartmentsId,
+                                                                    int DistrictsId);
+}
