502b722edffe3f3b318ae5ff60c125283175a178
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 18:27:27 -0500

    feat(publishing): implemented repository for department.

---
+++ RoadCareService/Publishing/Infrastructure/Persistence/EFC/Repositories/DepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using RoadCareService.Publishing.Domain.Model.Entities;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace RoadCareService.Publishing.Infrastructure.Persistence.EFC.Repositories
+{
+    public class DepartmentRepository(RoadCareContext context) :
+        BaseRepository<Department>(context), IDepartmentRepository
+    { }
+}
