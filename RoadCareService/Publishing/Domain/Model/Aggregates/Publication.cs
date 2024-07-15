891cfa3c0b31f538dc47d9254b7f4f3cb6e66287
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 17:18:22 -0500

    feat(publishing): creation contructor for the publication.

--- RoadCareService/Publishing/Domain/Model/Aggregates/Publication.cs
+++ RoadCareService/Publishing/Domain/Model/Aggregates/Publication.cs
@@ -1,5 +1,6 @@
 ﻿using RoadCareService.IAM.Domain.Model.Aggregates;
 using RoadCareService.Interaction.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Domain.Model.Commands;
 using RoadCareService.Publishing.Domain.Model.Entities;
 using RoadCareService.Publishing.Domain.Model.ValueObjects;
 
@@ -22,5 +23,34 @@ namespace RoadCareService.Publishing.Domain.Model.Aggregates
 
         public virtual ICollection<Comment> Comments { get; } = [];
         public virtual ICollection<Evidence> Evidences { get; } = [];
+
+        public Publication()
+        {
+            this.CitizensId = 0;
+            this.PublicationDate = DateTime.Now;
+            this.DistrictsId = 0;
+            this.Ubication = string.Empty;
+            this.Description = string.Empty;
+        }
+        public Publication(int citizensId, DateTime publicationDate,
+            int districtsId, string ubication, string description,
+            EPublicationState publicationState)
+        {
+            this.CitizensId = citizensId;
+            this.PublicationDate = publicationDate;
+            this.DistrictsId = districtsId;
+            this.Ubication = ubication;
+            this.Description = description;
+            this.PublicationState = publicationState;
+        }
+        public Publication(CreatePublicationCommand command)
+        {
+            this.CitizensId = command.CitizensId;
+            this.PublicationDate = DateTime.Now;
+            this.DistrictsId = command.DistrictsId;
+            this.Ubication = command.Ubication;
+            this.Description = command.Description;
+            this.PublicationState = command.PublicationState;
+        }
     }
 }
