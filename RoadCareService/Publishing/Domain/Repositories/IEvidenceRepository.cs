7eaa2179028c5970db17856a5d12753b765129c4
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 17:48:04 -0500

    feat(publishing): added repositories for publication and evidence.

---
+++ RoadCareService/Publishing/Domain/Repositories/IEvidenceRepository.cs
@@ -0,0 +1,11 @@
+﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Publishing.Domain.Repositories
+{
+    public interface IEvidenceRepository : IBaseRepository<Evidence>
+    {
+        Task<IEnumerable<Evidence>> FindByPublicationsIdAsync
+            (int publicationsId);
+    }
+}
---
+++ RoadCareService/Publishing/Domain/Repositories/IPublicationRepository.cs
@@ -0,0 +1,11 @@
+﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Publishing.Domain.Repositories
+{
+    public interface IPublicationRepository : IBaseRepository<Publication>
+    {
+        Task<IEnumerable<Publication>> FindByDepartmentsIdAndDistrictsIdAsync
+            (int departmentsId, int districtsId);
+    }
+}
