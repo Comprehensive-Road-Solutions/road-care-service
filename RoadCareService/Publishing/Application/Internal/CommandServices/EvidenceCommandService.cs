0eb21cbd1600fa4e20518c8d7824000af0107e20
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 19:56:52 -0500

    feat(publishing): implemented command service for evidence and publication.

---
+++ RoadCareService/Publishing/Application/Internal/CommandServices/EvidenceCommandService.cs
@@ -0,0 +1,29 @@
+﻿using RoadCareService.Publishing.Domain.Model.Commands.Evidence;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Publishing.Domain.Services.Evidence;
+using RoadCareService.Publishing.Infrastructure.Persistence.EFC.Repositories;
+using RoadCareService.Shared.Domain.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace RoadCareService.Publishing.Application.Internal.CommandServices
+{
+    public class EvidenceCommandService(RoadCareContext context,
+        IUnitOfWork unitOfWork) : IEvidenceCommandService
+    {
+        private IEvidenceRepository EvidenceRepository =
+            new EvidenceRepositoryEFC(context);
+
+        public async Task<bool> Handle(CreateEvidenceCommand command)
+        {
+            try
+            {
+                await EvidenceRepository.AddAsync(new(command));
+
+                await unitOfWork.CompleteAsync();
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
