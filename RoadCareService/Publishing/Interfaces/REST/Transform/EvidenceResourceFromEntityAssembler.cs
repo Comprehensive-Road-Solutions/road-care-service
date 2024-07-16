886a018bf51d22907b31c24b2333c60d44f77e14
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 20:41:46 -0500

    feat(publishing): added transform.

---
+++ RoadCareService/Publishing/Interfaces/REST/Transform/EvidenceResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Interfaces.REST.Resources;
+
+namespace RoadCareService.Publishing.Interfaces.REST.Transform
+{
+    public class EvidenceResourceFromEntityAssembler
+    {
+        public static EvidenceResource ToResourceFromEntity
+            (Evidence entity) => new(entity.Id,
+                                     entity.PublicationsId,
+                                     entity.FileUrl);
+    }
+}
---
+++ RoadCareService/Publishing/Interfaces/REST/Transform/PublicationResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Interfaces.REST.Resources;
+
+namespace RoadCareService.Publishing.Interfaces.REST.Transform
+{
+    public class PublicationResourceFromEntityAssembler
+    {
+        public static PublicationResource ToResourceFromEntity
+            (Publication entity) => new(entity.Id, entity.CitizensId,
+                                        entity.DistrictsId, entity.Ubication,
+                                        entity.Description);
+    }
+}
