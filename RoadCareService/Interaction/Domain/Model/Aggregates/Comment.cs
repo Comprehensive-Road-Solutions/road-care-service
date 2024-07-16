16ed0999e5e839736c602b80bd27030cafa5e675
aaron acuña <aaronacu_85@hotmail.com>
2024-07-16 16:43:55 -0500

    feat(interaction): added command for comment.

--- RoadCareService/Interaction/Domain/Model/Aggregates/Comment.cs
+++ RoadCareService/Interaction/Domain/Model/Aggregates/Comment.cs
@@ -1,4 +1,5 @@
 ﻿using RoadCareService.IAM.Domain.Model.Aggregates;
+using RoadCareService.Interaction.Domain.Model.Commands;
 using RoadCareService.Publishing.Domain.Model.Aggregates;
 
 namespace RoadCareService.Interaction.Domain.Model.Aggregates
@@ -14,5 +15,27 @@ namespace RoadCareService.Interaction.Domain.Model.Aggregates
 
         public virtual Citizen Citizens { get; } = null!;
         public virtual Publication Publications { get; } = null!;
+
+        public Comment()
+        {
+            this.PublicationsId = 0;
+            this.CitizensId = 0;
+            this.ShippingDate = DateTime.Now;
+            this.Opinion = string.Empty;
+        }
+        public Comment(int publicationsId, int citizensId, string opinion)
+        {
+            this.PublicationsId = publicationsId;
+            this.CitizensId = citizensId;
+            this.ShippingDate = DateTime.Now;
+            this.Opinion = opinion;
+        }
+        public Comment(AddCommentToPublicationCommand command)
+        {
+            this.PublicationsId = command.PublicationsId;
+            this.CitizensId = command.CitizensId;
+            this.ShippingDate = DateTime.Now;
+            this.Opinion = command.Opinion;
+        }
     }
 }
---
+++ RoadCareService/Interaction/Domain/Model/Commands/AddCommentToPublicationCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace RoadCareService.Interaction.Domain.Model.Commands
+{
+    public record AddCommentToPublicationCommand(int PublicationsId,
+                                                 int CitizensId,
+                                                 string Opinion);
+}
