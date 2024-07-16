76abdf2e12f9e3130dce9be9c238c0bf42a337a7
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 19:10:12 -0500

    feat(publishing): implemented repository for publication.

---
+++ RoadCareService/Publishing/Infrastructure/Persistence/Dapper/Repositories/PublicationRepositoryDapper.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System.Data;
+using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace RoadCareService.Publishing.Infrastructure.Persistence.Dapper.Repositories
+{
+    public class PublicationRepositoryDapper(RoadCareContext context, IDbConnection connection) :
+        BaseRepository<Publication>(context), IPublicationRepository
+    {
+        public async Task<IEnumerable<Publication>?> FindByDepartmentsIdAndDistrictsIdAsync
+            (int departmentsId, int districtsId)
+        {
+            var parameters = new DynamicParameters();
+
+            parameters.Add("@departments_id", departmentsId);
+            parameters.Add("@districts_id", departmentsId);
+
+            var result = await connection.QueryAsync<Publication>
+                ("sp_search_publication_by_departments_id_and_districts_id",
+                parameters, commandType: CommandType.StoredProcedure);
+
+            return result;
+        }
+    }
+}
---
+++ RoadCareService/Publishing/Infrastructure/Persistence/EFC/Repositories/PublicationRepositoryEFC.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace RoadCareService.Publishing.Infrastructure.Persistence.EFC.Repositories
+{
+    public class PublicationRepositoryEFC(RoadCareContext context) :
+        BaseRepository<Publication>(context), IPublicationRepository
+    {
+        public Task<IEnumerable<Publication>?> FindByDepartmentsIdAndDistrictsIdAsync
+            (int departmentsId, int districtsId) => throw new NotImplementedException();
+    }
+}
