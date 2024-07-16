98fdbbbd5774d53acd8fef416568bf3054e0b6f3
aaron acuña <aaronacu_85@hotmail.com>
2024-07-16 16:57:03 -0500

    feat(interaction): added repository for comment.

---
+++ RoadCareService/Interaction/Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using RoadCareService.Interaction.Domain.Model.Aggregates;
+using RoadCareService.Interaction.Domain.Model.Queries;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Interaction.Domain.Repositories
+{
+    public interface ICommentRepository : IBaseRepository<Comment>
+    {
+        Task<IEnumerable<Comment>?> FindByPublicationsIdAsync
+            (GetCommentsByPublicationsIdQuery query);
+    }
+}
