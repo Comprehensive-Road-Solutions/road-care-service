57fac76814a8502f5fce05aeddd46575954a6476
aaron acuña <aaronacu_85@hotmail.com>
2024-07-15 20:10:08 -0500

    feat(publishing): implemented queries services for evidence and publication.

---
+++ RoadCareService/Publishing/Application/Internal/QueryServices/EvidenceQueryService.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Domain.Model.Queries.Evidence;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Publishing.Domain.Services.Evidence;
+using RoadCareService.Publishing.Infrastructure.Persistence.Dapper.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace RoadCareService.Publishing.Application.Internal.QueryServices
+{
+    public class EvidenceQueryService(RoadCareContext context,
+        IDbConnection connection) : IEvidenceQueryService
+    {
+        private readonly IEvidenceRepository EvidenceRepository =
+            new EvidenceRepositoryDapper(context, connection);
+
+        public async Task<IEnumerable<Evidence>?> Handle
+            (GetEvidencesByPublicationsIdQuery query) =>
+            await EvidenceRepository
+            .FindByPublicationsIdAsync(query.PublicationsId);
+    }
+}
---
+++ RoadCareService/Publishing/Application/Internal/QueryServices/PublicationQueryService.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using RoadCareService.Publishing.Domain.Model.Aggregates;
+using RoadCareService.Publishing.Domain.Model.Queries.Publication;
+using RoadCareService.Publishing.Domain.Repositories;
+using RoadCareService.Publishing.Domain.Services.Publication;
+using RoadCareService.Publishing.Infrastructure.Persistence.Dapper.Repositories;
+using RoadCareService.Publishing.Infrastructure.Persistence.EFC.Repositories;
+using RoadCareService.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace RoadCareService.Publishing.Application.Internal.QueryServices
+{
+    public class PublicationQueryService(RoadCareContext context,
+        IDbConnection connection) : IPublicationQueryService
+    {
+        private IPublicationRepository? PublicationRepository;
+
+        public async Task<IEnumerable<Publication>?> Handle
+            (GetAllPublicationsQuery query)
+        {
+            PublicationRepository =
+                new PublicationRepositoryEFC(context);
+
+            return await PublicationRepository.ListAsync();
+        }
+        public async Task<IEnumerable<Publication>?> Handle
+            (GetPublicationsByDepartmentsIdAndDistrictsIdQuery query)
+        {
+            PublicationRepository =
+                new PublicationRepositoryDapper(context, connection);
+
+            return await PublicationRepository
+                .FindByDepartmentsIdAndDistrictsIdAsync
+                (query.DepartmentsId, query.DistrictsId);
+        }
+    }
+}
