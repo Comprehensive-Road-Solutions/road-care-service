1d93a33cd33e7dd0112b048e4928427d1316ac29
aaron acuña <aaronacu_85@hotmail.com>
2024-07-16 17:00:40 -0500

    feat(interaction): added services for comment.

---
+++ RoadCareService/Interaction/Domain/Services/ICommentCommandService.cs
@@ -0,0 +1,10 @@
+﻿using RoadCareService.Interaction.Domain.Model.Commands;
+
+namespace RoadCareService.Interaction.Domain.Services
+{
+    public interface ICommentCommandService
+    {
+        Task<bool> Handle
+            (AddCommentToPublicationCommand command);
+    }
+}
---
+++ RoadCareService/Interaction/Domain/Services/ICommentQueryService.cs
@@ -0,0 +1,11 @@
+﻿using RoadCareService.Interaction.Domain.Model.Aggregates;
+using RoadCareService.Interaction.Domain.Model.Queries;
+
+namespace RoadCareService.Interaction.Domain.Services
+{
+    public interface ICommentQueryService
+    {
+        Task<IEnumerable<Comment>?> Handle
+            (GetCommentsByPublicationsIdQuery query);
+    }
+}
