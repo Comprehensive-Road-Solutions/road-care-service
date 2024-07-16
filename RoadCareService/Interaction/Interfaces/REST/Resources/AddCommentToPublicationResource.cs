8813c5f771f259e5fcca3c31de920bb0e3bd8d97
aaron acuña <aaronacu_85@hotmail.com>
2024-07-16 17:37:33 -0500

    feat(publishing): added resources.

---
+++ RoadCareService/Interaction/Interfaces/REST/Resources/AddCommentToPublicationResource.cs
@@ -0,0 +1,6 @@
+﻿namespace RoadCareService.Interaction.Interfaces.REST.Resources
+{
+    public record AddCommentToPublicationResource(int PublicationsId,
+                                                  int CitizensId,
+                                                  string Opinion);
+}
---
+++ RoadCareService/Interaction/Interfaces/REST/Resources/CommentResource.cs
@@ -0,0 +1,5 @@
+﻿namespace RoadCareService.Interaction.Interfaces.REST.Resources
+{
+    public record CommentResource(int Id, int PublicationsId,
+                                  int CitizensId, string Opinion);
+}
