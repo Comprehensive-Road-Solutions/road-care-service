fd2a77e19a2161d1ca991468bedf54ffbc88c304
aaron acuña <aaronacu_85@hotmail.com>
2024-07-17 23:17:59 -0500

    feat(monitoring): added repositories for staff and damaged infrastructure.

---
+++ RoadCareService/Monitoring/Domain/Repositories/IDamagedInfrastructure.cs
@@ -0,0 +1,14 @@
+﻿using RoadCareService.Monitoring.Domain.Model.Aggregates;
+using RoadCareService.Monitoring.Domain.Model.ValueObjects.DamagedInfrastructure;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Monitoring.Domain.Repositories
+{
+    public interface IDamagedInfrastructure : IBaseRepository<DamagedInfrastructure>
+    {
+        Task<IEnumerable<DamagedInfrastructure>> FindByDepartmentsIdAndDistrictsIdAsync
+            (int departmentsId, int districtsId);
+        Task<IEnumerable<DamagedInfrastructure>> FindByStateAsync
+            (EDamagedInfrastructureState damagedInfrastructureState);
+    }
+}
---
+++ RoadCareService/Monitoring/Domain/Repositories/IStaffRepository.cs
@@ -0,0 +1,12 @@
+﻿using RoadCareService.Monitoring.Domain.Model.Aggregates;
+using RoadCareService.Monitoring.Domain.Model.ValueObjects.Staff;
+using RoadCareService.Shared.Domain.Repositories;
+
+namespace RoadCareService.Monitoring.Domain.Repositories
+{
+    public interface IStaffRepository : IBaseRepository<Staff>
+    {
+        Task<IEnumerable<Staff>> FindByStateAsync(EStaffState staffState);
+        Task<IEnumerable<Staff>> FindByWorkersIdAsync(int workersId);
+    }
+}
