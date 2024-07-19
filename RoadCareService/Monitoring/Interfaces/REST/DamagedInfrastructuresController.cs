849fcaf8b30239fa13291569dcce12c9800152d1
aaron acuña <aaronacu_85@hotmail.com>
2024-07-18 20:35:09 -0500

    feat(monitoring): added controller for staff and damaged infrastructure.

---
+++ RoadCareService/Monitoring/Interfaces/REST/DamagedInfrastructuresController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using RoadCareService.Monitoring.Domain.Services.DamagedInfrastructure;
+using RoadCareService.Monitoring.Interfaces.REST.Resources.DamagedInfrastructure;
+using RoadCareService.Monitoring.Interfaces.REST.Transform.DamagedInfrastructure;
+using RoadCareService.Monitoring.Domain.Model.Queries.DamagedInfrastructure;
+using RoadCareService.Monitoring.Domain.Model.ValueObjects.DamagedInfrastructure;
+
+namespace RoadCareService.Monitoring.Interfaces.REST
+{
+    [Route("api/damagedinfrastructures/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class DamagedInfrastructuresController
+        (IDamagedInfrastructureCommandService damagedInfrastructureCommandService,
+        IDamagedInfrastructureQueryService damagedInfrastructureQueryService) :
+        ControllerBase
+    {
+        [Route("register-damaged-infrastructure")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterDamagedInfrastructure
+            ([FromBody] RegisterDamagedInfrastructureResource resource)
+        {
+            var result = await damagedInfrastructureCommandService
+                .Handle(RegisterDamagedInfrastructureCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("update-damaged-infrastructure-state")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateDamagedInfrastructureState
+            ([FromBody] UpdateDamagedInfrastructureStateResource resource)
+        {
+            var result = await damagedInfrastructureCommandService
+                .Handle(UpdateDamagedInfrastructureStateCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("assign-work-date")]
+        [HttpPost]
+        public async Task<IActionResult> AssignWorkDateToDamagedInfrastructure
+            ([FromBody] AssignWorkDateToDamagedInfrastructureResource resource)
+        {
+            var result = await damagedInfrastructureCommandService
+                .Handle(AssignWorkDateToDamagedInfrastructureCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("damaged-infrastructures")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllDamagedInfrastructures()
+        {
+            var damagedInfrastructures = await damagedInfrastructureQueryService
+                .Handle(new GetAllDamagedInfrastructuresQuery());
+
+            if (damagedInfrastructures is null)
+                return BadRequest();
+
+            var damagedInfrastructuresResource = damagedInfrastructures
+                .Select(DamagedInfrastructureResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(damagedInfrastructuresResource);
+        }
+
+        [Route("damaged-infrastructures-by-id")]
+        [HttpGet]
+        public async Task<IActionResult> GetDamagedInfrastructuresById
+            ([FromQuery] int id)
+        {
+            var damagedInfrastructure = await damagedInfrastructureQueryService
+                .Handle(new GetDamagedInfrastructureByIdQuery(id));
+
+            if (damagedInfrastructure is null)
+                return BadRequest();
+
+            var damagedInfrastructureResource = 
+                DamagedInfrastructureResourceFromEntityAssembler
+                .ToResourceFromEntity(damagedInfrastructure);
+
+            return Ok(damagedInfrastructureResource);
+        }
+
+        [Route("damaged-infrastructures-by-department-and-district")]
+        [HttpGet]
+        public async Task<IActionResult> GetDamagedInfrastructuresByDepartmentsIdAndDistrictsId
+            ([FromQuery] int departmentsId, [FromQuery] int districtsId)
+        {
+            var damagedInfrastructures = await damagedInfrastructureQueryService
+                .Handle(new GetDamagedInfrastructuresByDepartmentsIdAndDistrictsIdQuery
+                (departmentsId, districtsId));
+
+            if (damagedInfrastructures is null)
+                return BadRequest();
+
+            var damagedInfrastructuresResource = damagedInfrastructures
+                .Select(DamagedInfrastructureResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(damagedInfrastructuresResource);
+        }
+
+        [Route("damaged-infrastructures-by-state")]
+        [HttpGet]
+        public async Task<IActionResult> GetDamagedInfrastructuresByState
+            ([FromQuery] string state)
+        {
+            var damagedInfrastructures = await damagedInfrastructureQueryService
+                .Handle(new GetDamagedInfrastructuresByStateQuery
+                (Enum.Parse<EDamagedInfrastructureState>(state)));
+
+            if (damagedInfrastructures is null)
+                return BadRequest();
+
+            var damagedInfrastructuresResource = damagedInfrastructures
+                .Select(DamagedInfrastructureResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(damagedInfrastructuresResource);
+        }
+    }
+}
---
+++ RoadCareService/Monitoring/Interfaces/REST/StaffController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using RoadCareService.Monitoring.Domain.Model.Queries.Staff;
+using RoadCareService.Monitoring.Domain.Services.Staff;
+using RoadCareService.Monitoring.Interfaces.REST.Transform.Staff;
+using RoadCareService.Monitoring.Interfaces.REST.Resources.Staff;
+using RoadCareService.Monitoring.Domain.Model.ValueObjects.Staff;
+
+namespace RoadCareService.Monitoring.Interfaces.REST
+{
+    [Route("api/staff/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class StaffController(IStaffCommandService staffCommandService,
+        IStaffQueryService staffQueryService) : ControllerBase
+    {
+        [Route("add-staff-in-charge")]
+        [HttpPost]
+        public async Task<IActionResult> AddStaffInCharge
+            ([FromBody] AddStaffInChargeResource resource)
+        {
+            var result = await staffCommandService
+                .Handle(AddStaffInChargeCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("update-staff-state")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaffState
+            ([FromBody] UpdateStaffStateResource resource)
+        {
+            var result = await staffCommandService
+                .Handle(UpdateStaffStateCommandFromResourceAssembler
+                .ToCommandFromResource(resource));
+
+            if (result is false)
+                return BadRequest();
+
+            return Ok(result);
+        }
+
+        [Route("staff")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllStaff()
+        {
+            var staff = await staffQueryService
+                .Handle(new GetAllStaffQuery());
+
+            if (staff is null)
+                return BadRequest();
+
+            var staffResource = staff.Select
+                (StaffResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(staffResource);
+        }
+
+        [Route("staff-by-id")]
+        [HttpGet]
+        public async Task<IActionResult> GetStaffById
+            ([FromQuery] int id)
+        {
+            var staff = await staffQueryService
+                .Handle(new GetStaffByIdQuery(id));
+
+            if (staff is null)
+                return BadRequest();
+
+            var staffResource =
+                StaffResourceFromEntityAssembler
+                .ToResourceFromEntity(staff);
+
+            return Ok(staffResource);
+        }
+
+        [Route("staff-by-state")]
+        [HttpGet]
+        public async Task<IActionResult> GetStaffByState
+            ([FromQuery] string state)
+        {
+            var staff = await staffQueryService
+                .Handle(new GetStaffByStateQuery
+                (Enum.Parse<EStaffState>(state)));
+
+            if (staff is null)
+                return BadRequest();
+
+            var staffResource = staff.Select
+                (StaffResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(staffResource);
+        }
+
+        [Route("staff-by-worker")]
+        [HttpGet]
+        public async Task<IActionResult> GetStaffByWorkersId
+            ([FromQuery] int workersId)
+        {
+            var staff = await staffQueryService
+                .Handle(new GetStaffByWorkersIdQuery
+                (workersId));
+
+            if (staff is null)
+                return BadRequest();
+
+            var staffResource = staff.Select
+                (StaffResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(staffResource);
+        }
+    }
+}
