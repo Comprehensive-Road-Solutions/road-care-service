bea840eadba73274c6e2c97d0e32223fcb7a37e1
aaron acuña <aaronacu_85@hotmail.com>
2024-07-20 16:20:49 -0500

    feat(assignment): added controller for government entity.

---
+++ RoadCareService/Assignment/Interfaces/REST/GovernmentsEntitiesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using RoadCareService.Assignment.Domain.Model.Queries.GovernmentEntity;
+using RoadCareService.Assignment.Domain.Services.GovernmentEntity;
+using RoadCareService.Assignment.Interfaces.REST.Transform.GovernmentEntity;
+
+namespace RoadCareService.Assignment.Interfaces.REST
+{
+    [Route("api/governmentsentities/")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class GovernmentsEntitiesController
+        (IGovernmentEntityQueryService governmentEntityQueryService) :
+        ControllerBase
+    {
+        [Route("all-governments-entities")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllGovernmentsEntities()
+        {
+            var govenmentsEntities = await governmentEntityQueryService
+                .Handle(new GetAllGovernmentsEntitiesQuery());
+
+            if (govenmentsEntities is null)
+                return BadRequest();
+
+            var govenmentsEntitiesResource = govenmentsEntities
+                .Select(GovernmentEntityResourceFromEntityAssembler
+                .ToResourceFromEntity);
+
+            return Ok(govenmentsEntitiesResource);
+        }
+
+        [Route("government-entity-by-id")]
+        [HttpGet]
+        public async Task<IActionResult> GetGovernmentEntityById
+            ([FromQuery] int id)
+        {
+            var govenmentEntity = await governmentEntityQueryService
+                .Handle(new GetGovernmentEntityByIdQuery(id));
+
+            if (govenmentEntity is null)
+                return BadRequest();
+
+            var govenmentEntityResource = GovernmentEntityResourceFromEntityAssembler
+                .ToResourceFromEntity(govenmentEntity);
+
+            return Ok(govenmentEntity);
+        }      
+    }
+}
