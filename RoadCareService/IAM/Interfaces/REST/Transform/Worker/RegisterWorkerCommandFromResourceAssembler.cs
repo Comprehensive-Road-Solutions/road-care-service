5dcfaf17e2e98c9f4c544d9951937702f3dbd059
aaron acuña <aaronacu_85@hotmail.com>
2024-07-22 16:41:55 -0500

    feat(iam): added transform for worker.

---
+++ RoadCareService/IAM/Interfaces/REST/Transform/Worker/RegisterWorkerCommandFromResourceAssembler.cs
@@ -0,0 +1,18 @@
+﻿using RoadCareService.IAM.Domain.Model.Commands.Worker;
+using RoadCareService.IAM.Domain.Model.ValueObjects.Worker;
+using RoadCareService.IAM.Interfaces.REST.Resources.Worker;
+
+namespace RoadCareService.IAM.Interfaces.REST.Transform.Worker
+{
+    public class RegisterWorkerCommandFromResourceAssembler
+    {
+        public static RegisterWorkerCommand ToCommandFromResource
+            (RegisterWorkerResource resource) =>
+            new(resource.Id, resource.DistrictId,
+                resource.GovernmentEntityId,
+                resource.Firstname, resource.Lastname,
+                resource.Age, resource.Genre,
+                resource.Phone, resource.Email,
+                resource.Address, EWorkerState.ACTIVO);
+    }
+}
---
+++ RoadCareService/IAM/Interfaces/REST/Transform/Worker/WorkerResourceFromEntityAssembler.cs
@@ -0,0 +1,16 @@
+﻿using RoadCareService.IAM.Interfaces.REST.Resources.Worker;
+
+namespace RoadCareService.IAM.Interfaces.REST.Transform.Worker
+{
+    public class WorkerResourceFromEntityAssembler
+    {
+        public static WorkerResource ToResourceFromEntity
+            (Domain.Model.Aggregates.Worker entity) =>
+                new(entity.Id, entity.DistrictsId,
+                entity.GovernmentsEntitiesId,
+                entity.Firstname, entity.Lastname,
+                entity.Age, entity.Genre,
+                entity.Phone, entity.Email,
+                entity.Address);
+    }
+}
