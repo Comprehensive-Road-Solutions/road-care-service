097e593745ab4f87043c3f3aaebb0f1c50d0cd3a
aaron acuña <aaronacu_85@hotmail.com>
2024-07-22 16:33:20 -0500

    feat(iam): added transform for citizen.

---
+++ RoadCareService/IAM/Interfaces/REST/Transform/Citizen/CitizenResourceFromEntityAssembler.cs
@@ -0,0 +1,14 @@
+﻿using RoadCareService.IAM.Interfaces.REST.Resources.Citizen;
+
+namespace RoadCareService.IAM.Interfaces.REST.Transform.Citizen
+{
+    public class CitizenResourceFromEntityAssembler
+    {
+        public static CitizenResource ToResourceFromEntity
+            (Domain.Model.Aggregates.Citizen entity) =>
+            new(entity.Id, entity.ProfileUrl,
+                entity.Firstname, entity.Lastname,
+                entity.Age,entity.Genre,
+                entity.Phone, entity.Email);
+    }
+}
---
+++ RoadCareService/IAM/Interfaces/REST/Transform/Citizen/RegisterCitizenCommandFromResourceAssembler.cs
@@ -0,0 +1,17 @@
+﻿using RoadCareService.IAM.Domain.Model.Commands.Citizen;
+using RoadCareService.IAM.Domain.Model.ValueObjects.Citizen;
+using RoadCareService.IAM.Interfaces.REST.Resources.Citizen;
+
+namespace RoadCareService.IAM.Interfaces.REST.Transform.Citizen
+{
+    public class RegisterCitizenCommandFromResourceAssembler
+    {
+        public static RegisterCitizenCommand ToCommandFromResource
+            (RegisterCitizenResource resource) =>
+            new(resource.Id, resource.ProfileUrl,
+                resource.Firstname, resource.Lastname,
+                resource.Age, resource.Genre,
+                resource.Phone, resource.Email,
+                ECitizenState.ACTIVO);
+    }
+}
