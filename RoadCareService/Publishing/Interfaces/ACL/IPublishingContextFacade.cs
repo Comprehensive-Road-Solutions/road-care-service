86c7f2673d5a9f86a2971f340cc46765a516d556
aaron acuña <aaronacu_85@hotmail.com>
2024-08-07 18:09:36 -0500

    fix(publishing): corrected facade.

--- RoadCareService/Publishing/Interfaces/ACL/IPublishingContextFacade.cs
+++ RoadCareService/Publishing/Interfaces/ACL/IPublishingContextFacade.cs
@@ -1,14 +1,7 @@
-﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
-
-namespace RoadCareService.Publishing.Interfaces.ACL
+﻿namespace RoadCareService.Publishing.Interfaces.ACL
 {
     public interface IPublishingContextFacade
     {
-        Task<IEnumerable<Publication>?> GetAllPublications();
-
-        Task<IEnumerable<Evidence>?> GetEvidencesByPublicationId
-            (int publicationId);
-
         Task<bool> ExistsPublicationById
             (int id);
     }
--- RoadCareService/Publishing/Interfaces/ACL/Services/PublishingContextFacade.cs
+++ RoadCareService/Publishing/Interfaces/ACL/Services/PublishingContextFacade.cs
@@ -1,28 +1,15 @@
-﻿using RoadCareService.Publishing.Domain.Model.Aggregates;
-using RoadCareService.Publishing.Domain.Model.Queries.Evidence;
-using RoadCareService.Publishing.Domain.Model.Queries.Publication;
-using RoadCareService.Publishing.Domain.Services.Evidence;
+﻿using RoadCareService.Publishing.Domain.Model.Queries.Publication;
 using RoadCareService.Publishing.Domain.Services.Publication;
 
 namespace RoadCareService.Publishing.Interfaces.ACL.Services
 {
     internal class PublishingContextFacade
-        (IPublicationQueryService publicationQueryService,
-        IEvidenceQueryService evidenceQueryService) :
+        (IPublicationQueryService publicationQueryService) :
         IPublishingContextFacade
     {
-        public async Task<IEnumerable<Publication>?> GetAllPublications() =>
-            await publicationQueryService
-            .Handle(new GetAllPublicationsQuery());
-
         public async Task<bool> ExistsPublicationById
             (int id) => await publicationQueryService
             .Handle(new GetPublicationByIdQuery
                 (id)) != null;
-
-        public async Task<IEnumerable<Evidence>?> GetEvidencesByPublicationId
-            (int publicationId) => await evidenceQueryService
-            .Handle(new GetEvidencesByPublicationIdQuery
-                (publicationId));
     }
 }
